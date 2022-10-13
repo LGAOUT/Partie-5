f14a43f0f36f59a5cff3bce5a903f0312713ca9b
Yassin Lgaout <yassinlgaout@gmail.com>
2022-10-13 23:12:15 +0200

    feat(package): eslint & prettier & tDypescript config

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (_, res) => {
-    res.send('Bonjour !');
+app.get("/", (_, res) => {
+  res.send("Bonjour !");
 });
 
-app.listen(port,()=>{
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
